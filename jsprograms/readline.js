ab9d0749100ec0f6edc08b1126a4d647fe2c7aa8
kennyfrc <fxkennyfrc@gmail.com>
2016-02-02 17:42:24 +0800

    added some files

--- jsprograms/readline.js
+++ jsprograms/readline.js
@@ -2,11 +2,10 @@ var readline = require('readline');
 
 var rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
-  terminal: false
+  output: process.stdout
 });
 
 rl.question(">>What's your name?  ", function(answer) {
-   console.log("Hello " + answer);
+   console.log("Hello " + answer + "! It's nice to meet you!");
    rl.close();
 });
